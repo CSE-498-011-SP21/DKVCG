6731946d467daeaa88ac9760444d95ecf8bc5a48
Olivia Grimes <oliviagrimes@Olivias-MacBook-Pro.local>
2021-04-18 17:39:13 -0400

    updated client.cu to take in and parse csv test file, and use discoverPrimary when cached primary is wrong

--- service/client.cu
+++ service/client.cu
@@ -10,7 +10,7 @@
 namespace ft = cse498::faulttolerance;
 int LOG_LEVEL = WARNING;
 
-void sendBatchAndRecvResponse(cse498::Connection *client,
+bool sendBatchAndRecvResponse(cse498::Connection *client,
                               std::vector<RequestWrapper<unsigned long long, data_t *>> &clientBatch,
                               cse498::unique_buf &buf) {
     std::vector<char> serializedData;
@@ -27,10 +27,13 @@ void sendBatchAndRecvResponse(cse498::Connection *client,
         if (v.size() + sizeof(size_t) + serializedData.size() >= 4096) {
             size_t size = serializedData.size();
             buf.cpyTo((char *) &size, sizeof(size_t));
-            client->send(buf, sizeof(size_t));
+            client->async_send(buf, sizeof(size_t));
             buf.cpyTo(serializedData.data(), serializedData.size());
-            client->send(buf, serializedData.size());
+            client->async_send(buf, serializedData.size());
             serializedData.clear();
+            if (!client->try_wait_for_sends()) {
+                return false;
+            }
         }
 
         serializedData.insert(serializedData.end(), v.begin(), v.end());
@@ -40,10 +43,13 @@ void sendBatchAndRecvResponse(cse498::Connection *client,
     if (!serializedData.empty()) {
         size_t size = serializedData.size();
         buf.cpyTo((char *) &size, sizeof(size_t));
-        client->send(buf, sizeof(size_t));
+        client->async_send(buf, sizeof(size_t));
         buf.cpyTo(serializedData.data(), serializedData.size());
-        client->send(buf, serializedData.size());
+        client->async_send(buf, serializedData.size());
         serializedData.clear();
+        if (!client->try_wait_for_sends()) {
+            return false;
+        }
     }
 
     auto start = std::chrono::high_resolution_clock::now();
@@ -53,6 +59,8 @@ void sendBatchAndRecvResponse(cse498::Connection *client,
     auto end = std::chrono::high_resolution_clock::now();
 
     std::cout << 512 / std::chrono::duration<double>(end - start).count() << " Ops" << std::endl;
+
+    return true;
 }
 
 void endConnection(cse498::Connection *client, cse498::unique_buf &buf) {
@@ -61,13 +69,39 @@ void endConnection(cse498::Connection *client, cse498::unique_buf &buf) {
     client->send(buf, sizeof(size_t));
 }
 
+std::map<ft::Shard*, std::vector<RequestWrapper<unsigned long long, data_t *>>*> openTestFile(std::string file, ft::Client* ftClient) {
+    std::map<ft::Shard*, std::vector<RequestWrapper<unsigned long long, data_t *>>*> batches;
+    fstream fin;
+    // TODO: deal w failure if unable to open
+    fin.open(file, ios::in);
+
+    int reqInt;
+    unsigned long long key;
+    data_t value;
+    
+    while(getline(file, key, ',')) {
+        getline(file, value, ',');
+        getline(file, reqInt);
+        
+        ft::Shard* shard = ftClient->getShard((unsigned long long) key);
+
+        if (batches.find(shard) == batches.end()) {
+            batches.insert({shard, new std::vector<RequestWrapper<unsigned long long, data_t *>>});
+        }
+        batches.find(shard)->second->push_back({key, 0, value, reqInt});
+    }
+
+    return batches;
+}
+
 int main(int argc, char **argv) {
     std::string cfgFile;
+    std::string testFile = nullptr;
     bool ftEnabled = true;
     ft::Client* ftClient = new ft::Client();
 
     char c;
-    while ((c = getopt(argc, argv, "vf:")) != -1) {
+    while ((c = getopt(argc, argv, "vft:")) != -1) {
         switch (c) {
             case 'v':
                 LOG_LEVEL++;
@@ -75,6 +109,10 @@ int main(int argc, char **argv) {
             case 'f':
                 cfgFile = optarg;
                 // optarg is the file
+                break;
+            case 't':
+                testFile = testData;
+                // testData is csv file containing test data
                 break;
         }
     }
@@ -92,24 +130,33 @@ int main(int argc, char **argv) {
 
     if (ftEnabled) {
         std::map<ft::Shard*, std::vector<RequestWrapper<unsigned long long, data_t *>>*> batches;
-        for (int i = 0; i < 512; i++) {
-            ft::Shard* shard = ftClient->getShard((unsigned long long) (i + 1));
+        if (testFile == nullptr) {
+            for (int i = 0; i < 512; i++) {
+                ft::Shard* shard = ftClient->getShard((unsigned long long) (i + 1));
 
-            if (batches.find(shard) == batches.end()) {
-                batches.insert({shard, new std::vector<RequestWrapper<unsigned long long, data_t *>>});
+                if (batches.find(shard) == batches.end()) {
+                    batches.insert({shard, new std::vector<RequestWrapper<unsigned long long, data_t *>>});
+                }
+                batches.find(shard)->second->push_back({(unsigned long long) (i + 1), 0, new data_t(32), REQUEST_INSERT});
             }
-            batches.find(shard)->second->push_back({(unsigned long long) (i + 1), 0, new data_t(32), REQUEST_INSERT});
+        } else {
+            batches = openTestFile(testFile, ftClient);
         }
+        
 
         for (auto it = batches.begin(); it != batches.end(); ++it) {
             auto client = it->first->getPrimary()->primary_conn;
+            // TODO: would line below fail if no longer primary..? or not till below in sendBatchAndRecvResponse
             client->connect();
 
             cse498::unique_buf* buf = new cse498::unique_buf();
             uint64_t key = 1;
             client->register_mr(*buf, FI_READ | FI_WRITE | FI_SEND | FI_RECV, key);
 
-            sendBatchAndRecvResponse(client, *it->second, *buf);
+            if (!sendBatchAndRecvResponse(client, *it->second, *buf)) {
+                it->first->discoverPrimary();
+                client = it->first->getPrimary()->primary_conn;
+            }
             endConnection(client, *buf);
         }
     }
